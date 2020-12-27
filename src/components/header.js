cca9c10e64af34ebcab192efb9c8ab4e3ce81bf5
Tyler Maschino <tylermaschino@gmail.com>
2020-12-27 12:39:08 -0500

    Committing....stuff

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,17 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react";
+import { Link } from "gatsby";
+
+import "./header.css";
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div>
+    <div className="nav">
+      <Link to="/">{siteTitle}</Link>
+      <Link to="/about/">About</Link>
+      <Link to="/services/">Services</Link>
+      <Link to="/contact/">Contact</Link>
     </div>
   </div>
-)
+);
 
-export default Header
+export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,28 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from '../components/layout'
+import Layout from "../components/layout";
+import "./index.css";
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="hero">
+      <h1 className="hero-text">
+        Shaking Up Your Site and Improving Your Web Presence
+      </h1>
+      <button>Learn More</button>
+      <button>Contact Us</button>
+    </div>
+    <div className="why">
+      <h1>Why Shake Up Your Site?</h1>
+    </div>
+    <div className="how">
+      <h1>How We Do It</h1>
+    </div>
+    <div className="what">
+      <h1>What To Expect</h1>
+    </div>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
